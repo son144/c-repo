9009d09a723a472dd21aa125979ada40073d00f1
bisht65190 <bisht65190@gmail.com>
2024-04-10 09:05:02 +0530

    first commit

---
+++ Hello.c
@@ -0,0 +1,141 @@
+#include<stdio.h>
+
+// #define number1 10
+
+int main(){
+
+// printf("%d",number1);
+printf("hello\n");
+
+// operators 
+// arithmetic 
+// int a=10;
+// int b=20;
+// int sum=a+b;
+// printf("sum is %d",sum);
+
+//  int a=10;
+// int b=20;
+// int sum=a-b;
+// printf("minus ans is %d",sum);
+
+//  int a=10;
+// int b=20;
+// int mul=a*b;
+// printf("mul ans is %d",mul);
+
+
+//  int a=10;
+// int b=2;
+// int divide=a/b;
+// printf("divide ans is %d",divide);
+
+//  int a=10;
+// int b=3;
+// int remainder=a%b;
+// printf("remainder ans is %d",remainder);
+
+
+// relational operators 
+// int a=4;
+// int b=3;
+// printf("ans is %d",a>b);
+
+
+// int a=4;
+// int b=3;
+// printf("ans is %d",a<b);
+
+
+// int a=4;
+// int b=3;
+// printf("ans is %d",a<=b);
+
+// int a=3;
+// int b=3;
+// printf("ans is %d",a>=b);
+
+
+// int a=3;
+// int b=3;
+// printf("ans is %d",a==b);
+
+// int a=4;
+// int b=3;
+// printf("ans is %d",a!=b);
+
+
+// logical operators 
+// &&
+// int isSunday=1;
+// int isRaining=0;
+// printf("lets go out %d\n",isSunday&&isRaining);
+
+
+// int isSunday=0;
+// int isRaining=0;
+// int isFree=0;
+// printf("lets go out %d\n",isSunday||isRaining||isFree);
+
+
+// int isSunday=0;
+// int isRaining=0;
+// int isHoliday=1  ;
+// printf("lets go out %d\n",((isSunday||isHoliday)&&isRaining));
+
+// const declaration 
+
+// const int number=10;
+// printf("%d",number);
+
+// assignment 
+// int num=10;
+// int num2=20;
+// num=num+num2;
+// num+=num2;
+// printf("%d",num);
+
+// num-=num2;
+// num=num-num2;
+
+
+// *=
+// num*=num2;
+
+// /=
+
+// num/=num2;
+// num=num/num2;
+
+// %=
+// num%=num2;
+
+// printf("%d",num);
+
+
+// int num=10;
+// float num2=5.5;
+
+// // int sum=num+num2;
+// printf("ans is %d",num);
+
+// explicit type conversion 
+int num=10;
+float num2=5.5;
+float sum=(float)num+num2;                                                                                                                                                                                                                                                                                                                                                                     
+
+// imlicit type conversion 
+int num=10;
+int num2=5.5;
+float sum=num+num2;
+printf("ans is %f",sum);
+
+// int num=14.6;
+// printf("%d",num);
+// float num=15;
+// printf("%f",num);
+
+
+
+}
+
